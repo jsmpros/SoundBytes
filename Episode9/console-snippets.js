bcb0de93e741f9cb6ff70d7c6b8d62de14b5ceea
Jim Marion <jim.marion@jsmpros.com>
2020-02-25 21:23:15 -0800

    Episode 9 Code

---
+++ Episode9/console-snippets.js
@@ -0,0 +1,5 @@
+/* expand tagged item */
+document.querySelector('.jsm_expanded .ps-link').click();
+
+/* expand first item */
+document.querySelector(".ps_accordion section:first-child a").click();
